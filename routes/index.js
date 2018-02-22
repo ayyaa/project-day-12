400e9091557d45f74e26d2d6bc7b0066c29e0bec
Qisti <qisti.rahmah@gmail.com>
2018-02-22 14:33:14 +0700

    edit index.js at routes directory

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Project Day 12' });
+  res.render('index', { title: 'Project Day 12 !!!' });
 });
 
 module.exports = router;
